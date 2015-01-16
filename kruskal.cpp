d481f4f479bc01ed85ad15af1d722d046ce9568f
yuguang <zh.yuguang.cn@gmail.com>
2015-01-16 10:45:27 -0500

    implement kruskal's algorithm in python

--- kruskal.cpp
+++
@@ -1,9 +0,0 @@
-# include "tnode.h"
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-	tnode * node = new tnode(0);
-	delete node;
-
-	std::cout << "Hello world" << std::endl;
-}
---
+++ kruskal.py
@@ -0,0 +1,36 @@
+def find(component, u):
+    if not u in component:
+        return u
+    if component[u] != u:
+        component[u] = find(component, component[u])
+    return component[u]
+
+def combine(component, rank, u, v):
+    u, v = find(component, u), find(component, v)
+    if rank[u] > rank[v]:
+        component[v] = u
+    else:
+        component[u] = v
+    if rank[u] == rank[v]:
+        rank[v] += 1
+
+def kruskal(edges, component, rank):
+    tree = set()
+    for _, u, v in sorted(edges):
+        if find(component, u) != find(component, v):
+            tree.add((u, v))
+            combine(component, rank, u, v)
+    return tree
+
+with open('g10.22.edg', 'r') as f:
+    (num_edges, num_vertices) = [int(item) for item in f.readline().split(' ')]
+    next(f)
+    component = {}
+    edges = []
+    for line in f:
+        (u, v, weight) = [int(item) for item in line.split(' ')]
+        component[u] = u
+        component[v] = v
+        edges.append((weight, u, v))
+    rank = {u: 0 for u in range(0, num_edges)}
+    print kruskal(edges, component, rank)
